296de1378713b4c35388464602adfdd70bd20a47
Matt Sicker <boards@gmail.com>
2018-03-22 12:43:39 -0500

    Add more type utility functions

--- jvzgo/src/types/types.go
+++ jvzgo/src/types/types.go
@@ -1,6 +1,16 @@
 package types
 
+import "fmt"
+
 type MalType interface {
+}
+
+func NewTypeError(expectedType string, actual MalType) error {
+	return fmt.Errorf("unexpected type; expected %v; actual value: %v", expectedType, actual)
+}
+
+func RaiseTypeError(expectedType string, actual MalType) (MalType, error) {
+	return nil, NewTypeError(expectedType, actual)
 }
 
 type MalList struct {
@@ -11,6 +21,36 @@ type MalList struct {
 
 func NewList(values ...MalType) MalList {
 	return MalList{Value: values, StartStr: "(", EndStr: ")"}
+}
+
+func IsList(val MalType) bool {
+	switch list := val.(type) {
+	case MalList:
+		return list.StartStr == "(" && list.EndStr == ")"
+	default:
+		return false
+	}
+}
+
+func NewVec(values ...MalType) MalList {
+	return MalList{Value: values, StartStr: "[", EndStr: "]"}
+}
+
+func IsVec(val MalType) bool {
+	switch list := val.(type) {
+	case MalList:
+		return list.StartStr == "[" && list.EndStr == "]"
+	default:
+		return false
+	}
+}
+
+func GetSlice(val MalType) ([]MalType, error) {
+	list, ok := val.(MalList)
+	if !ok {
+		return nil, fmt.Errorf("provided value is not sliceable: %v", val)
+	}
+	return list.Value, nil
 }
 
 type MalMap struct {
