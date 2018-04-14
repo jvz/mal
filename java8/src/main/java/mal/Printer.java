501ed73aff997e55640fa2c5dad04fc05c7a2085
Matt Sicker <boards@gmail.com>
2018-04-14 13:34:09 -0500

    Add start of step 1

--- java8/src/main/java/mal/Printer.java
+++ java8/src/main/java/mal/Printer.java
@@ -1,5 +1,11 @@
 package mal;
 
+import mal.Types.MalType;
+
 public class Printer {
+
+    public static String pr_str(MalType value) {
+        return value.print(true);
+    }
 
 }
--- java8/src/main/java/mal/Types.java
+++ java8/src/main/java/mal/Types.java
@@ -1,5 +1,111 @@
 package mal;
 
-public class Types {
+import java.util.List;
+import java.util.stream.Collectors;
 
+public class Types {
+    public interface MalType {
+        Object value();
+
+        default MalType meta() {
+            return null;
+        }
+
+        default String print(boolean pretty) {
+            Object value = value();
+            return value == null ? "nil" : value.toString();
+        }
+    }
+
+    public enum MalNil implements MalType {
+        NIL;
+
+        @Override
+        public Void value() {
+            return null;
+        }
+    }
+
+    public enum MalBool implements MalType {
+        FALSE(false),
+        TRUE(true);
+
+        private final boolean value;
+
+        MalBool(boolean value) {
+            this.value = value;
+        }
+
+        @Override
+        public Boolean value() {
+            return value;
+        }
+    }
+
+    public static class MalInt implements MalType {
+        private final int value;
+
+        public MalInt(int value) {
+            this.value = value;
+        }
+
+        @Override
+        public Integer value() {
+            return value;
+        }
+    }
+
+    private static abstract class AbstractStringType implements MalType {
+        final String value;
+
+        private AbstractStringType(String value) {
+            this.value = value;
+        }
+
+        @Override
+        public String value() {
+            return value;
+        }
+    }
+
+    public static class MalSymbol extends AbstractStringType {
+        public MalSymbol(String value) {
+            super(value);
+        }
+    }
+
+    public static class MalString extends AbstractStringType {
+        public MalString(String value) {
+            super(value);
+        }
+    }
+
+    public static class MalKeyword extends AbstractStringType {
+        public MalKeyword(String value) {
+            super(value);
+        }
+
+        @Override
+        public String print(boolean pretty) {
+            return ':' + value;
+        }
+    }
+
+    public static class MalList implements MalType {
+        private final List<MalType> value;
+
+        public MalList(List<MalType> value) {
+            this.value = value;
+        }
+
+        @Override
+        public Object value() {
+            return value;
+        }
+
+        @Override
+        public String print(boolean pretty) {
+            return value.stream().map(v -> v.print(pretty)).collect(Collectors.joining(" ", "(", ")"));
+        }
+    }
 }
